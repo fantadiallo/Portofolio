020c31c5f42ea7c03e1599e2f2773931808dce1a
fantadiallo <fatmbo0003@outlook.com>
2025-06-08 23:13:24 +0200

    Update App.jsx to include section scroll handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Home from './pages/Home/Home';
 import Project1Page from './pages/Project1Page/Project1Page';
 import Project2Page from './pages/Project2Page/Project2Page';
 import Project3Page from './pages/Project3Page/Project3Page';
+import ScrollToHash from './components/ScrollToHash';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects/project-1" element={<Project1Page />} />
-          <Route path="/projects/project-2" element={<Project2Page />} />
-          <Route path="/projects/project-3" element={<Project3Page />} />
-        </Route>
-      </Routes>
+      <>
+        <ScrollToHash />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects/project-1" element={<Project1Page />} />
+            <Route path="/projects/project-2" element={<Project2Page />} />
+            <Route path="/projects/project-3" element={<Project3Page />} />
+          </Route>
+        </Routes>
+      </>
     </Router>
   );
 }
- 
