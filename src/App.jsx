6c625f220a50e9200198a7f8393ac600715176d1
fantadiallo <fatmbo0003@outlook.com>
2025-07-29 15:32:50 +0200

    Wire Freelance component into routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Project1Page from './pages/Project1Page/Project1Page';
-import Project2Page from './pages/Project2Page/Project2Page';
 import Project3Page from './pages/Project3Page/Project3Page';
 import ScrollToHash from './components/ScrollToHash';
+import Freelance from './components/freelance/freelance';
 
 export default function App() {
   return (
@@ -15,7 +15,6 @@ export default function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/projects/project-1" element={<Project1Page />} />
-            <Route path="/projects/project-2" element={<Project2Page />} />
             <Route path="/projects/project-3" element={<Project3Page />} />
           </Route>
         </Routes>
